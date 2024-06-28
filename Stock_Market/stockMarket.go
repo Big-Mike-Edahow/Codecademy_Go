1ca6283f1845da8a55e748963df892f8d21d56df
Mike Jackson <perigran_falcon@yahoo.com>
2024-06-28 17:59:01 -0600

    Add files via upload

---
+++ Stock_Market/stockMarket.go
@@ -0,0 +1,45 @@
+/* stockMarket.go */
+
+package main
+
+import (
+	"fmt"
+	"math/rand"
+	"time"
+)
+
+type Stock struct {
+	name  string
+	price float32
+}
+
+func (s *Stock) updateStock() {
+	change := randomNumberGen(-10000, 10000)
+	s.price += change
+}
+
+func displayMarket(market []Stock) {
+	for i := 0; i < len(market); i++ {
+		fmt.Println(market[i])
+	}
+}
+
+func randomNumberGen(min float32, max float32) float32 {
+	return min + (max-min)*rand.Float32()
+}
+
+func main() {
+	rand.Seed(time.Now().UnixNano())
+
+	GOOG := Stock{"GOOG", 2313.50}
+	AAPL := Stock{"AAPL", 157.28}
+	FB := Stock{"FB", 203.77}
+	TWTR := Stock{"TWTR", 50.00}
+
+	stockMarket := []Stock{GOOG, AAPL, FB, TWTR}
+
+	displayMarket(stockMarket)
+	stockMarket[0].updateStock()
+	stockMarket[1].updateStock()
+	displayMarket(stockMarket)
+}
